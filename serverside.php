920ef37e136d31b764c1afe00110bf24ee91e013
juniaraldi <58576752+juniaraldi@users.noreply.github.com>
2021-06-07 16:40:28 +0700

    edit js

---
+++ serverside.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" />
+    <title>Datables</title>
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link">Client Slide</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="serverside.php" class="nav-link"> Server Side</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container mt-3">
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-9">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Tabel Tinggi Permukaan Air - Server Side</h5>
+                            <hr>
+                            <table id="tabel-data" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nilai</th>
+                                        <th>Waktu</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#tabel-data').DataTable({
+                "processing" : true,
+                "serverside" : true,
+                "ajax" : "data.php",
+                "columnDefs" : [{"render": tombol, "data":null, "targets":[3]}]
+            });
+        });
+
+        function tombol(){
+            return "<a href='dbedit.php?id_tma=$row[id_tma]' class='btn btn-info'>Edit</a> <a href='dbdelete.php?id_tma=$row[id_tma]' class='btn btn-danger'>Hapus</a>";
+        }
+    </script>
+  </body>
+</html>
